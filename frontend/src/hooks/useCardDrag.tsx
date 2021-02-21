abab8251b5024f5074ce187fd8a7db1d03eef753
NillsvanLimbeek <nills.van.limbeek@live.nl>
2021-02-21 14:46:48 +0100

    cleanup

--- frontend/src/hooks/useCardDrag.tsx
+++ frontend/src/hooks/useCardDrag.tsx
@@ -1,32 +1,8 @@
 import { DropResult } from 'react-beautiful-dnd';
 
 import { IColumn } from '../data/types/Column';
-// import { ICard } from '../data/types/Card';
 
 export const useCardDrag = () => {
-    // const updateDraggedCards = (
-    //     columns: IColumn[],
-    //     cards: ICard[],
-    //     result: DropResult,
-    // ) => {
-    //     const { source, destination, draggableId } = result;
-
-    //     // drop in the same column
-    //     if (source.droppableId === destination?.droppableId) {
-    //         // new columnIds
-    //         const newCardIds = Array.from(column.cardIds);
-    //         newCardIds.splice(source.index, 1);
-    //         newCardIds.splice(destination.index, 0, draggableId);
-
-    //         // new columns order
-    //         const filteredCards: any = newCardIds.map((id) => {
-    //             return cards.find((card) => card.id === id);
-    //         });
-
-    //         return filteredCards;
-    //     }
-    // };
-
     // new column
     const dragInSameColumn = (column: IColumn, result: DropResult) => {
         const { source, destination, draggableId } = result;
